0e4fc3740b9a9b3a7aaa37abc068aeec4aa6ed66
Meloncommelefruit <147407024+Meloncommelefruit@users.noreply.github.com>
2023-10-10 12:56:23 +0200

    Add files via upload

---
+++ exo7/main.js
@@ -0,0 +1,13 @@
+ let prenoms = []
+ 
+ prenoms.push("Vincent","Paul","Arthur")
+ 
+
+// tableau vide de base 
+// ajouter prénoms dans le tableau 
+
+prenoms.forEach(prenom=> {
+    prenom += "va à la pêche" ;
+    console.log(prenom)
+}) //mettre va à la pêche a chaque fin d'élément
+
