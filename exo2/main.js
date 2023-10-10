886743e2761050af638bc1f2a2345c8e8634522f
Meloncommelefruit <147407024+Meloncommelefruit@users.noreply.github.com>
2023-10-10 18:51:45 +0200

    Add files via upload

---
+++ exo2/main.js
@@ -0,0 +1,15 @@
+let a = 4
+let b =4
+let c =3
+
+switch(a){
+    case b:
+        console.log('égal à B')
+        break
+
+    case c:
+        console.log('égale à C')
+        break
+    default:
+        console.log('égal à rien')
+}
