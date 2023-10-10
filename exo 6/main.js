d59c31d68a758b7dd8e4e1e6a8ef6242122f9faa
Meloncommelefruit <147407024+Meloncommelefruit@users.noreply.github.com>
2023-10-10 11:08:24 +0200

    Add files via upload

---
+++ exo 6/main.js
@@ -0,0 +1,12 @@
+let a ="jean"
+let b="Paul"
+
+function checkName(name1,name2) {
+    if (name1==name2) {
+        return true
+    } else {
+        return false
+    }
+}
+let result=checkName (a,b)
+console.log (result)
